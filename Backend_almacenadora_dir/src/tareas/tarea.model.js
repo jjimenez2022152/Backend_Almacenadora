dc32b40c2e5181a0359f2bd2e2410894d8485796
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-04-29 14:48:13 -0600

    se sube backend

---
+++ Backend_almacenadora_dir/src/tareas/tarea.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const tareaSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "El nombre de la tarea es obligatorio"],
+    },
+    descripcion: {
+        type: String,
+        required: [true, "La descripción de la tarea es obligatoria"],
+    },
+    fechaInicio: {
+        type: Date,
+        required: [true, "La fecha de inicio de la tarea es obligatoria"],
+    },
+    fechaCierre: {
+        type: Date,
+        required: [true, "La fecha de cierre de la tarea es obligatoria"],
+    },
+    estadoTarea: {
+        type: String,
+        enum: ["completa", "incompleta"],
+        default: "incompleta",
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+    creador: {
+        type: String,
+        required: [true, "El nombre del creador de la tarea es obligatorio"],
+    },
+});
+
+export default mongoose.model('Tarea', tareaSchema);
